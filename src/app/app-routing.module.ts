0509b599414d243c9033357a30ae9a95ab2ea718
JOSE MARIA RICAURTE MORA <1031182625@bolivar.com>
2022-11-05 19:21:43 -0500

    Router funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { DirectivasComponent } from './components/directivas/directivas.component';
+import { LazyLoadingComponent } from './components/lazy-loading/lazy-loading.component';
+import { ObserverComponent } from './components/observer/observer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:DataBindingComponent},
+  {path:'directivas', component:DirectivasComponent},
+  {path:'observer', component:ObserverComponent},
+  {path:'lazyloading', component:LazyLoadingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
