5d2290f1ee1d8f5b84cb88e72121f7d53230a3f1
Wilson Hurtado <wilson.hurtado@segurosbolivar.com>
2022-11-08 13:41:11 -0500

    Directivas estructurales listas!

--- src/app/components/directivas/directivas.component.ts
+++ src/app/components/directivas/directivas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/persona.model';
 
 @Component({
   selector: 'app-directivas',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivasComponent implements OnInit {
 
+  inputIf: string = "";
+  persona: Persona = {
+    nombre: '',
+    apellido: '',
+    edad: 0,
+    correo: ''
+  }
+  personas: Persona[] = [
+    {
+      nombre: 'Wilson David',
+      apellido: 'Hurtado Morales',
+      edad: 25,
+      correo: 'wilson.hurtado@segurosbolivar.com'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addRegister(){
+    this.personas.push(this.persona)
   }
 
 }
