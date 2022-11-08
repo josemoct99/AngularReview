30deba9617f0f345b0609a3842cd560e21daa890
JOSE MARIA RICAURTE MORA <1031182625@bolivar.com>
2022-11-07 23:51:06 -0500

    LazyLoading funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { DirectivasComponent } from './components/directivas/directivas.component';
-import { LazyLoadingComponent } from './components/lazy-loading/lazy-loading.component';
 import { ObserverComponent } from './components/observer/observer.component';
 
 const routes: Routes = [
   {path:'', component:DataBindingComponent},
   {path:'directivas', component:DirectivasComponent},
   {path:'observer', component:ObserverComponent},
-  {path:'lazyloading', component:LazyLoadingComponent}
+  {path:'lazyloading', loadChildren: ()=> import('./components/lazy-loading/lazy-loading-routing.module').then(m=>m.LazyLoadingRoutingModule)}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { DirectivasComponent } from './components/directivas/directivas.component';
 import { ObserverComponent } from './components/observer/observer.component';
-import { LazyLoadingComponent } from './components/lazy-loading/lazy-loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
     DirectivasComponent,
-    ObserverComponent,
-    LazyLoadingComponent
+    ObserverComponent
   ],
   imports: [
     BrowserModule,
